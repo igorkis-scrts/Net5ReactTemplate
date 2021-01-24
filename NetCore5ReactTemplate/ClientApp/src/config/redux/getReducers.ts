f964423e07385fc7f6d63b4def098ce0e7d41df5
Igor Kiselyov <igorkiselyov74@live.com>
2021-01-24 17:35:43 +0300

    forecast store

--- NetCore5ReactTemplate/ClientApp/src/config/redux/getReducers.ts
+++ NetCore5ReactTemplate/ClientApp/src/config/redux/getReducers.ts
@@ -5,14 +5,16 @@ import { ReducersMapObject } from "redux";
 import { State } from "@State";
 import { NotifierReducer } from "@app/Notifier/redux/Notifier.reducer";
 import { HistoryReducer } from "@utils/redux/history/History.reducer";
+import { ForecastsReducer } from "@app/Forecasts/redux/Forecasts.reducer";
 
 import { AppProviderReducer } from "@config/components/AppProvider/redux/AppProvider.reducer";
 
 export function getReducers(history: History): ReducersMapObject<State, any> {
-    return {
-        router: connectRouter(history),
-        historyStore: HistoryReducer,
-        appProviderStore: AppProviderReducer,
-        notifierStore: NotifierReducer,
-    };
+   return {
+      router: connectRouter(history),
+      historyStore: HistoryReducer,
+      appProviderStore: AppProviderReducer,
+      notifierStore: NotifierReducer,
+      forecastsStore: ForecastsReducer
+   };
 }
