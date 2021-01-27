3585aaced6cba667ab0303e362d18006fa73a1cb
Igor Kiselyov <igorkiselyov74@live.com>
2021-01-27 21:39:25 +0300

    additional aliases

--- NetCore5ReactTemplate/ClientApp/craco.config.js
+++ NetCore5ReactTemplate/ClientApp/craco.config.js
@@ -4,8 +4,11 @@ const tsNameof = require("ts-nameof");
 module.exports = {
     webpack: {
         alias: {
+            ...this.alias,
+            '@api': path.resolve(__dirname, "src/api/"),
             '@app': path.resolve(__dirname, "src/app/"),
             '@utils': path.resolve(__dirname, "src/utils/"),
+            '@models': path.resolve(__dirname, "src/app/models/"),
             '@config': path.resolve(__dirname, "src/config/"),
             '@shared': path.resolve(__dirname, "src/app/shared/"),
             '@State': path.resolve(__dirname, "src/config/redux/State")
@@ -21,7 +24,7 @@ module.exports = {
                                 loader: require.resolve('ts-loader'),
                                 options: {
                                     transpileOnly: true,
-                                    getCustomTransformers: () => ({ before: [tsNameof] }),
+                                    getCustomTransformers: () => ({before: [tsNameof]}),
                                 },
                             },
                         ],
